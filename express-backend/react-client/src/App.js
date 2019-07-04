cb75ed5951b6e984788b47356818e833dddf4205
travis <travis.borsa@gmail.com>
2019-07-03 21:39:55 -0700

    pokesockets stripped of the sockets for demo

--- express-backend/react-client/src/App.js
+++ express-backend/react-client/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Battle from './Battle';
 import Pokedex from './Pokedex';
 import Attacker from './Attacker';
-import Menu from './Menu';
-import openSocket from 'socket.io-client';
+
 import './index.css';
 import './App.css';
 import './Battle.css';
@@ -14,16 +12,15 @@ class App extends React.Component {
     super(props);
     this.state = {
       opponentOne:{
-          number: null,
+          number: 20,
           health: 100,
           attacked: false
       },
       opponentTwo:{
           number: null,
-          health: 100,
+          health: 150,
           attacked: false
-      },
-      menu: "attack"
+      }
     }
   }
   setPokemon = (pokeNum) => {
@@ -33,38 +30,13 @@ class App extends React.Component {
     }
     this.setState({[ opponent ]: { number: pokeNum, health: this.state[opponent].health, attacked: false}})
   }
-  
-  sendAttack = (opponent) => {
-    this.socket.emit('attack', {opponent: opponent});
-  }
-  
-  componentDidMount(){
-    this.socket = openSocket('http://supercoolstuff:3000');
-    this.socket.on('attack', (data)=>{
-      console.log(data);
-      if(data.opponent == 1 ){
-          this.setState({opponentOne: { number: this.state.opponentOne.number, health: this.state.opponentOne.health -1, attacked: true}}, ()=>{
-            setTimeout(()=>{ 
-              this.setState({opponentOne: { number: this.state.opponentOne.number, health: this.state.opponentOne.health, attacked: false}})
-            }, 500);
-          })
-      }else{
-          this.setState({opponentTwo: { number: this.state.opponentTwo.number, health: this.state.opponentTwo.health -1, attacked: true}}, ()=>{
-            setTimeout(()=>{ 
-              this.setState({opponentTwo: { number: this.state.opponentTwo.number, health: this.state.opponentTwo.health, attacked: false}})
-            }, 500);
-          })
-      }
-    });
-    this.socket.emit('attack', {opponent: 1});
-  }
 
   render(){
     return (
         <div className="app">
           {this.state.menu ==="battle" && !this.state.opponentTwo.number &&<Pokedex setPokemon={this.setPokemon}></Pokedex> }
           {this.state.menu ==="battle" && this.state.opponentTwo.number && <Battle opponentOne={this.state.opponentOne} opponentTwo={this.state.opponentTwo}></Battle>}
-          {this.state.menu ==="attack" && <Attacker sendAttack={this.sendAttack} ></Attacker>}
+          {this.state.menu ==="attack" && <Attacker></Attacker>}
         </div>
       );
   }
--- express-backend/socketServer.js
+++ express-backend/socketServer.js
@@ -1,13 +1,1 @@
-module.exports = function(io) {
-  io.on('connection', function(socket) {
-    console.log("connection");
-    socket.on('message', function(message) {
-      socket.emit('incommingMessage',message.value);
-      console.log('from console',message.value);
-    });
-    socket.on('attack', function(message) {
-      console.log(`opponent ${message.opponent} was attacked`);
-      socket.broadcast.emit('attack', message);
-    });
-  });
-};
+
